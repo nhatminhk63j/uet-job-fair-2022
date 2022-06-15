5c07be02e6b7ff7cd085f71bba29ee2869b9ebc7
Nhật Ngô Sách <nhat.ns@teko.vn>
2022-06-15 12:21:58 +0700

    commit

--- src/pages/HomePage/components/Sponsors/index.js
+++ src/pages/HomePage/components/Sponsors/index.js
@@ -25,61 +25,65 @@ const Sponsors = () => {
       {/*Diamond sponsors*/}
       <h4 className={'sponsor-title'}>NHÀ TÀI TRỢ KIM CƯƠNG</h4>
       <Row className={'justify-content-center'}>
-        {!isLoading && sponsor && sponsor.diamond?.map(sponsor => (<Col xs={12} md={6} xl={3}>
-          <div className="sponsor-card d-flex flex-column mt-5">
-            <h5><a href={`/sponsors/${sponsor.id}`}>{sponsor.name}</a>
-            </h5>
-            <img src={sponsor.logo} alt={sponsor.name} />
-          </div>
-        </Col>))}
+        {!isLoading && sponsor &&
+            sponsor.diamond?.map(sponsor => (<Col xs={12} md={6} xl={3}>
+              <div className="sponsor-card mt-5">
+                <h5><a href={`/sponsors/${sponsor.id}`}>{sponsor.name}</a></h5>
+                <img src={sponsor.logo} alt={sponsor.name} />
+              </div>
+            </Col>))}
       </Row>
 
       {/*Gold sponsors*/}
       <h4 className={'sponsor-title mt-5'}>NHÀ TÀI TRỢ VÀNG</h4>
       <Row className={'justify-content-center'}>
-        {!isLoading && sponsor && sponsor.gold?.map(sponsor => (<Col xs={12} md={6} xl={3}>
-          <div className="sponsor-card d-flex flex-column mt-5">
-            <h5><a href={`/sponsors/${sponsor.id}`}>{sponsor.name}</a>
-            </h5>
-            <img src={sponsor.logo} alt={sponsor.name} />
-          </div>
-        </Col>))}
+        {!isLoading && sponsor &&
+            sponsor.gold?.map(sponsor => (<Col xs={12} md={6} xl={3}>
+              <div className="sponsor-card d-flex flex-column mt-5">
+                <h5><a href={`/sponsors/${sponsor.id}`}>{sponsor.name}</a>
+                </h5>
+                <img src={sponsor.logo} alt={sponsor.name} />
+              </div>
+            </Col>))}
       </Row>
 
       {/*Silver sponsors*/}
       <h4 className={'sponsor-title mt-5'}>NHÀ TÀI TRỢ BẠC</h4>
       <Row className={'justify-content-center'}>
-        {!isLoading && sponsor && sponsor.silver?.map(sponsor => (<Col xs={12} md={6} xl={3}>
-          <div className="sponsor-card d-flex flex-column mt-5">
-            <h5><a href={`/sponsors/${sponsor.id}`}>{sponsor.name}</a>
-            </h5>
-            <img src={sponsor.logo} alt={sponsor.name} />
-          </div>
-        </Col>))}
+        {!isLoading && sponsor &&
+            sponsor.silver?.map(sponsor => (<Col xs={12} md={6} xl={3}>
+              <div className="sponsor-card d-flex flex-column mt-5">
+                <h5><a href={`/sponsors/${sponsor.id}`}>{sponsor.name}</a>
+                </h5>
+                <img src={sponsor.logo} alt={sponsor.name} />
+              </div>
+            </Col>))}
       </Row>
 
       {/*Copper sponsors*/}
       <h4 className={'sponsor-title mt-5'}>NHÀ TÀI TRỢ ĐỒNG</h4>
       <Row className={'justify-content-center'}>
-        {!isLoading && sponsor && sponsor.copper?.map(sponsor => (<Col xs={12} md={6} xl={3}>
-          <div className="sponsor-card d-flex flex-column mt-5">
-            <h5><a href={`/sponsors/${sponsor.id}`}>{sponsor.name}</a>
-            </h5>
-            <img src={sponsor.logo} alt={sponsor.name} />
-          </div>
-        </Col>))}
+        {!isLoading && sponsor &&
+            sponsor.copper?.map(sponsor => (<Col xs={12} md={6} xl={3}>
+              <div className="sponsor-card d-flex flex-column mt-5">
+                <h5><a href={`/sponsors/${sponsor.id}`}>{sponsor.name}</a>
+                </h5>
+                <img src={sponsor.logo} alt={sponsor.name} />
+              </div>
+            </Col>))}
       </Row>
 
       {/*Companion sponsors*/}
       <h4 className={'sponsor-title mt-5'}>DOANH NGHIỆP ĐỒNG HÀNH</h4>
       <Row className={'justify-content-center'}>
-        {!isLoading && sponsor && sponsor.companion?.map(sponsor => (<Col xs={12} md={6} xl={3}>
-          <div className="sponsor-card d-flex flex-column mt-5">
-            <h5><a href={`/sponsors/${sponsor.id}`}>{sponsor.name}</a>
-            </h5>
-            <img src={sponsor.logo} alt={sponsor.name} />
-          </div>
-        </Col>))}
+        {!isLoading && sponsor &&
+            sponsor.companion?.map(sponsor => (<Col xs={12} md={6} xl={3}>
+              <div className="sponsor-card d-flex flex-column mt-5">
+                <h5><a href={`/sponsors/${sponsor.id}`}>{sponsor.name}</a>
+                </h5>
+                <img src={sponsor.logo} alt={sponsor.name} />
+              </div>
+            </Col>))}
       </Row>
     </Container>
   </div>;
