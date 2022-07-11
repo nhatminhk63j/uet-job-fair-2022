d33ec43818bb59b3dc6e5bb869055dcdaebd73d3
Nhật Ngô Sách <nhat.ns@teko.vn>
2022-07-11 15:29:40 +0700

    change backend url

--- src/services/apis/request.js
+++ src/services/apis/request.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const apiUrl = 'https://uet-jobfair.site';
+const apiUrl = 'https://uethackathon.com';
 // const apiUrl = 'http://localhost:8080'
 const jobFairClient = axios.create({
   baseURL: apiUrl,
